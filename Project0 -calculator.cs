1930c5e44ce88fee788f46e4fca2cb4327719527
Travis <tboskowitz@gmail.com>
2021-11-27 10:25:32 -0600

    Calculator

---
+++ Project0 -calculator.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please type the expression you would like to evaluate and jot Enter.");
+string userInput = Console.ReadLine();
